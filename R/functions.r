04d11df911efbaedda78ae71f547a54b75c9f498
Martina Cusinato <martina.cusinato@bristol.ac.uk>
2026-02-02 15:56:24 +0000

    added documenation

--- R/functions.r
+++ R/functions.r
@@ -1,4 +1,25 @@
+#' Extract participant ID from TCGA barcode
+#' 
+#' ddjjdjd
+#' 
+#'
+#' @param id TCGA barcode
+#' @return a string represents the participant ID
+#' @export
+extract.participant <- function(id) {
+  sub("TCGA-[^-]+-([^-]+)-.*", "\\1", id)
+}
+
+#' Write table
+#' 
+#' ?
+#' 
+#' @param x data framte to write 
+#' @param filename output file name 
+#' @return none
+#' @export
 my_write_table <- function(x, filename) {
   cat("saving", basename(filename), "...\n")
   write.table(x, file = filename, row.names = T, col.names = T, sep = "\t")
+  print("hello")
 }
